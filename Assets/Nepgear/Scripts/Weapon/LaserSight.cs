8ded6c6850ecbde4665c18c63bc9874e1aeb8492
devilmustcry <eaglization@gmail.com>
2018-04-14 18:59:21 +0700

    Change to Lasersight instead of crosshair

---
+++ Assets/Nepgear/Scripts/Weapon/LaserSight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSight : MonoBehaviour {
+
+    private LineRenderer lr;
+    public float maxDistance = 100f;
+	// Use this for initialization
+	void Start () {
+        lr = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            lr.SetPosition(1, new Vector3(0, 0, hit.distance + 50f));
+        }
+        else
+        {
+            lr.SetPosition(1, new Vector3(0, 0, maxDistance));
+        }
+	}
+}
