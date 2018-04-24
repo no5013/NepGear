a6dbb5f2aba094a9f7b72f7bf67eca43945a034f
Pongsakorn Somsri <pongsakorns@gmail.com>
2018-04-24 18:39:42 +0700

    VR KEYBOARD

---
+++ Assets/Script/UIManagers/VRUI/VRKeyboard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VRKeyboard : MonoBehaviour
+{
+    public InputField input;
+
+    public void ClickKey(string character)
+    {
+        input.text += character;
+    }
+
+    public void Backspace()
+    {
+        if (input.text.Length > 0)
+        {
+            input.text = input.text.Substring(0, input.text.Length - 1);
+        }
+    }
+
+    public void Enter()
+    {
+        input.text = "";
+    }
+}
