431ede0c1064c9144574b1bb4da788343e7cfbab
Pongsakorn Somsri <pongsakorns@gmail.com>
2018-05-02 05:32:29 +0700

    Fix everything that I can

--- Assets/Script/Managers/GameManager.cs
+++ Assets/Script/Managers/GameManager.cs
@@ -228,7 +228,7 @@ public class GameManager : NetworkBehaviour {
             yield return null;
 
         //wait to be sure that all are ready to start
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(3.0f);
 
         //yield return StartCoroutine(RoundSetup());
 
